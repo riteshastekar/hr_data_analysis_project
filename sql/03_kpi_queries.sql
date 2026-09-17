4d7b7a836e0c979f82e81febf5e9cb205f68e19d
Ritesh Raju Astekar <riteshastekar40@gmail.com>
2026-09-17 17:09:14 +0530

    Create views for employee demographics and turnover

--- sql/03_kpi_queries.sql
+++ sql/03_kpi_queries.sql
@@ -1,19 +1,22 @@
 -- 1.WHAT IS THE GENDER BREAKDOWN OF EMPLOYEE IN THE COMPANY
+CREATE VIEW gender AS
 SELECT gender,COUNT(*) AS count_gen
 FROM hr
 WHERE termdate IS NULL
 GROUP BY gender 
-ORDER BY count_gen DESC
+
 
 -- 2.WHAT IS THE RACE BREAKDOWN OF THE EMPLOYEE IN THE COMPANY
+CREATE VIEW race_breakdown AS
 SELECT race,COUNT(*) AS count_race
 FROM hr
 WHERE termdate IS NULL
 GROUP BY race 
-ORDER BY count_race DESC
+
 
 -- 3.WHAT IS THE AGE DISTRIBUTION OF EMPLOYEES IN COMPANY
-SELECT age_group,COUNT(*) FROM
+CREATE VIEW age_distribution AS
+SELECT age_group,COUNT(*) as counts FROM
 (SELECT *,
 	CASE	
 		WHEN age>=18 AND age<=24 THEN '18-24'
@@ -26,79 +29,153 @@ END AS age_group
 FROM hr
 WHERE termdate IS NULL ) o
 GROUP BY age_group
-ORDER BY age_group
+
 
 -- 4. HOW MANY EMPLOYEES WORK AT HQ VS REMOTE
-SELECT location,COUNT(*) FROM hr
+CREATE VIEW  hq_VS_re AS
+SELECT location,COUNT(*) AS counts FROM hr
 WHERE termdate IS NULL
 GROUP BY location
 
 
---5. WHAT IS AVERAGE LENGTH OF THE EMPLOYMENT WHO HAVE BEEN TERMINATED
-SELECT ROUND(AVG(DATEDIFF(YEAR,hire_date,termdate)),0) AS length_of_employment FROM hr
-WHERE termdate IS NOT NULL AND termdate <= GETDATE()
+-- 5. WHAT IS AVERAGE LENGTH OF THE EMPLOYMENT WHO HAVE BEEN TERMINATED
+CREATE VIEW vw_avg_length_of_employment AS
+SELECT ROUND(AVG(DATEDIFF(YEAR, hire_date, termdate)), 0) AS length_of_employment
+FROM hr
+WHERE termdate IS NOT NULL AND termdate <= GETDATE();
+GO
 
 
---6.HOW DOES GENDER DISTRIBUTION VARY ACROSS dept. and jobtitle
-SELECT department,jobtitle,gender,COUNT(*) AS counts 
+-- 6. HOW DOES GENDER DISTRIBUTION VARY ACROSS DEPT. AND JOBTITLE
+CREATE VIEW vw_gender_distribution_by_dept_jobtitle AS
+SELECT department, jobtitle, gender, COUNT(*) AS counts
 FROM hr
 WHERE termdate IS NULL
-GROUP BY department,jobtitle,gender
-ORDER BY department,jobtitle,gender
+GROUP BY department, jobtitle, gender;
+GO
+--by DEPT ONLY
+CREATE VIEW vw_gender_distribution_by_dept AS
+SELECT department, gender, COUNT(*) AS counts
+FROM hr
+WHERE termdate IS NULL
+GROUP BY department,gender;
+GO
 
 
 -- 7. WHAT IS THE DISTRIBUTION OF JOBTITLE ACROSS THE COMPANY
-
-SELECT jobtitle,COUNT(*) AS counts 
+CREATE VIEW vw_jobtitle_distribution AS
+SELECT jobtitle, COUNT(*) AS counts
 FROM hr
 WHERE termdate IS NULL
-GROUP BY jobtitle
-ORDER BY counts DESC
+GROUP BY jobtitle;
+GO
+
 
 -- 8. WHICH DEPT HAS THE HIGHER TURNOVER/TERMINATION RATE
+CREATE VIEW vw_termination_rate_by_dept AS
 SELECT  department,
-		COUNT(*) AS Total_count,
+		COUNT(*) AS total_count,
 		COUNT(CASE
-				   WHEN termdate IS NOT NULL AND termdate <= GETDATE() THEN 1 
-				   END)AS terminated_count,
-		ROUND(COUNT(CASE 
-						 WHEN termdate IS NOT NULL AND termdate <= GETDATE() THEN 1 
-						 END)*100.0/COUNT(*),2) AS termination_rate
+				   WHEN termdate IS NOT NULL AND termdate <= GETDATE() THEN 1
+				   END) AS terminated_count,
+		ROUND(COUNT(CASE
+						 WHEN termdate IS NOT NULL AND termdate <= GETDATE() THEN 1
+						 END) * 100.0 / COUNT(*), 2) AS termination_rate
 FROM hr
-GROUP BY department
-ORDER BY termination_rate DESC
+GROUP BY department;
+GO
+
 
 -- 9. WHAT IS THE DISTRIBUTION OF EMPLOYEES ACROSS LOCATION_STATE
-SELECT location_state,COUNT(*) AS counts
+CREATE VIEW vw_employee_distribution_by_state AS
+SELECT location_state, COUNT(*) AS counts
 FROM hr
 WHERE termdate IS NULL
-GROUP BY location_state
+GROUP BY location_state;
+GO
+
 -- BY CITY
-SELECT location_city,COUNT(*) AS counts
+CREATE VIEW vw_employee_distribution_by_city AS
+SELECT location_city, COUNT(*) AS counts
 FROM hr
 WHERE termdate IS NULL
-GROUP BY location_city
+GROUP BY location_city;
+GO
 
--- 10. HOW HAS THE COMPANY'S EMPLOYEE COUNT CHANGED OVER TIME BASED ON hire and termination date
 
+-- 10. HOW HAS THE COMPANY'S EMPLOYEE COUNT CHANGED OVER TIME BASED ON HIRE AND TERMINATION DATE
+CREATE VIEW vw_employee_count_change_over_time AS
 SELECT years,
 	   hires,
 	   terminations,
-	   hires-terminations AS net_change,
-	   (terminations*100.0 /hires)AS change_percent
-FROM (SELECT YEAR(hire_date) AS years,
-			 COUNT(*) AS hires,
-			 SUM(CASE
-				   WHEN termdate IS NOT NULL AND termdate <= GETDATE() THEN 1 
-				   END)AS terminations
-			FROM hr
-			GROUP BY YEAR(hire_date)
-		)AS subquery
-ORDER BY years;
-	
---11.WHAT IS THE TENURE DISTRIBUTION FOR EACH DEPARTMENT
-SELECT department,ROUND(AVG(DATEDIFF(YEAR,hire_date,termdate)),0) AS avg_tenure
+	   hires - terminations AS net_change,
+	   (terminations*100.0/hires) AS change_percent
+FROM (
+	SELECT YEAR(hire_date) AS years,
+		   COUNT(*) AS hires,
+		   SUM(CASE
+				   WHEN termdate IS NOT NULL AND termdate <= GETDATE() THEN 1
+				   END) AS terminations
+	FROM hr
+	GROUP BY YEAR(hire_date)
+) AS subquery;
+GO
+
+
+-- 11. WHAT IS THE TENURE DISTRIBUTION FOR EACH DEPARTMENT
+CREATE VIEW vw_avg_tenure_by_dept AS
+SELECT department, ROUND(AVG(DATEDIFF(YEAR, hire_date, termdate)), 0) AS avg_tenure
 FROM hr
 WHERE termdate IS NOT NULL AND termdate <= GETDATE()
-GROUP BY department
-ORDER BY avg_tenure
+GROUP BY department;
+GO
+
+
+
+-- 12.  termination and hire breakdown gender wise
+CREATE VIEW vw_gender_wise_terminations_hires AS
+SELECT gender,
+	   total_hires,
+	   total_terminations,
+	   ROUND((total_terminations*100.0/total_hires),2) AS change_percent
+FROM (
+	SELECT gender,
+		   COUNT(*) AS total_hires,
+		   SUM(CASE
+				   WHEN termdate IS NOT NULL AND termdate <= GETDATE() THEN 1
+				   END) AS total_terminations
+	FROM hr
+	GROUP BY gender
+) AS subquery
+
+-- 13.  termination and hire breakdown age wise
+CREATE VIEW vw_age_wise_terminations_hires AS
+SELECT age,
+	   total_hires,
+	   total_terminations,
+	   ROUND((total_terminations*100.0/total_hires),2) AS change_percent
+FROM (
+	SELECT age,
+		   COUNT(*) AS total_hires,
+		   SUM(CASE
+				   WHEN termdate IS NOT NULL AND termdate <= GETDATE() THEN 1
+				   END) AS total_terminations
+	FROM hr
+	GROUP BY age
+) AS subquery
+
+-- 14.  termination and hire breakdown race wise
+CREATE VIEW vw_race_wise_terminations_hires AS
+SELECT race,
+	   total_hires,
+	   total_terminations,
+	   ROUND((total_terminations*100.0/total_hires),2) AS termination_rate
+FROM (
+	SELECT race,
+		   COUNT(*) AS total_hires,
+		   SUM(CASE
+				   WHEN termdate IS NOT NULL AND termdate <= GETDATE() THEN 1
+				   END) AS total_terminations
+	FROM hr
+	GROUP BY race
+) AS subquery
